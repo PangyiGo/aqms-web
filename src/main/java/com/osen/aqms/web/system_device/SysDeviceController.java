e2410f7ac9d5d0328124534ef3f1dc222914cf6f
tinypy <1306359812@qq.com>
2019-12-19 15:18:35 +0800

    commit info

--- src/main/java/com/osen/aqms/web/system_device/SysDeviceController.java
+++ src/main/java/com/osen/aqms/web/system_device/SysDeviceController.java
@@ -4,9 +4,11 @@ import com.osen.aqms.common.model.DeviceTreeModel;
 import com.osen.aqms.common.result.RestResult;
 import com.osen.aqms.common.utils.RestResultUtil;
 import com.osen.aqms.common.utils.SecurityUtil;
+import com.osen.aqms.modules.entity.system.Device;
 import com.osen.aqms.modules.service.DeviceService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +41,18 @@ public class SysDeviceController {
         List<DeviceTreeModel> treeList = deviceService.findDeviceTreeListToUsername(username);
         // 返回
         return RestResultUtil.success(treeList);
+    }
+
+    /**
+     * 根据设备号查询设备详细信息
+     *
+     * @return 信息
+     */
+    @PostMapping("/device/infoDetail/{deviceNo}")
+    public RestResult getDeviceInfo(@PathVariable("deviceNo") String deviceNo) {
+        Device oneDeviceToNo = deviceService.findOneDeviceToNo(deviceNo);
+        if (oneDeviceToNo == null)
+            return RestResultUtil.failed("无法查询设备");
+        return RestResultUtil.success(oneDeviceToNo);
     }
 }
