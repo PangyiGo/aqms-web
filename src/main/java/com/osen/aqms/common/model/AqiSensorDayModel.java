9320d7b95bde60960334a63a17cef100a24a52cc
tinypy <1306359812@qq.com>
2020-01-02 23:35:44 +0800

    commit

---
+++ src/main/java/com/osen/aqms/common/model/AqiSensorDayModel.java
@@ -0,0 +1,27 @@
+package com.osen.aqms.common.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * User: PangYi
+ * Date: 2020-01-02
+ * Time: 17:29
+ * Description:
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AqiSensorDayModel {
+
+    private BigDecimal number = new BigDecimal(0);
+
+    @JsonFormat(pattern = "MM-dd")
+    private LocalDateTime dateTime;
+
+}
