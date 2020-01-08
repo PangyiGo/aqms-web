e65de7f7b575c045e91437e55a19dbbf4cbb5c3d
tinypy <1306359812@qq.com>
2020-01-08 10:11:55 +0800

    commit info

--- src/main/java/com/osen/aqms/web/restful/model/AirResponseModel.java
+++ src/main/java/com/osen/aqms/web/restful/model/AirResponseModel.java
@@ -9,27 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AirResponseModel {
 
-    // 请求成功编码
-    public static Integer OK = 2002;
-
-    // 请求失败编码
-    public static Integer UNOK = 2004;
-
     private Integer code;
 
     private String msg;
-
-    public static AirResponseModel success(String command) {
-        AirResponseModel airResponseModel = new AirResponseModel();
-        airResponseModel.setCode(OK);
-        airResponseModel.setMsg("result command: " + command);
-        return airResponseModel;
-    }
-
-    public static AirResponseModel fail(String error) {
-        AirResponseModel airResponseModel = new AirResponseModel();
-        airResponseModel.setCode(UNOK);
-        airResponseModel.setMsg("error: " + error);
-        return airResponseModel;
-    }
 }
